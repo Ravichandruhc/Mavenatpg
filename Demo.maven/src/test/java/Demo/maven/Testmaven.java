d431bddd5c121c6d26c6813159e7c70387e21258
Ravi_Chandru <Ravi_Chandru@DESKTOP-GSD7FUT>
2018-04-10 22:13:58 +0530

    1st stage commit

---
+++ Demo.maven/src/test/java/Demo/maven/Testmaven.java
@@ -0,0 +1,14 @@
+package Demo.maven;
+
+import org.testng.annotations.Test;
+
+public class Testmaven 
+{
+	@Test
+	public void testmaven1()
+	{
+		System.out.println("Welcome to maven");
+		
+	}
+
+}
